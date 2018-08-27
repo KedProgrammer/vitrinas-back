49e38a68a489074ca8a26e3f7f2c4f0d5b36afa8
Daniel Tobon <shiriux1@gmail.com>
2018-08-27 13:52:49 -0500

    auth skeleton added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    post 'auth/login', to: 'authentication#authenticate'
+    post 'signup', to: 'users#create'
+  end
 end
