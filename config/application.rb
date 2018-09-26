44349b64a294588e9717fca96ccbf56b11f8f800
daniel <shiriux1@gmail.com>
2018-09-25 23:36:09 -0500

    added time zone

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module Orders
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.time_zone = 'Bogota'
+   
   end
 end
