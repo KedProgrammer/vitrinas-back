8e4c27c4d19cce00f36b9b0bf07be5b1d416dd23
Daniel <shirixu1@gmail.com>
2019-07-09 01:20:53 -0500

    fixed controller

--- app/controllers/v1/admin/product_row_materials_controller.rb
+++ app/controllers/v1/admin/product_row_materials_controller.rb
@@ -2,7 +2,7 @@ class V1::Admin::ProductRowMaterialsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     row_material = RowMaterial.find(params[:row_material_id])
-    product.product_row_materials.create(row_material: row_material, quantity: product_row_material[:quantity])
+    product.product_row_materials.create(row_material: row_material, quantity: product_row_materials_params[:quantity])
     render json: product
   end
 
