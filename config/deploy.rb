93a69cdf37074b30fda3141ed15320ab2fcdfda9
Daniel <shirixu1@gmail.com>
2019-08-28 20:29:51 -0500

    fixed capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,22 @@
-lock "~> 3.11.0"
+lock '3.11.0'
 
 set :application, 'vitrinas-back'
-set :repo_url, 'git@github.com:KedProgrammer/vitrinas-back.git' # Edit this to match your repository
-set :branch, :master
+set :repo_url, 'git@github.com:KedProgrammer/vitrinas-back.git'
+set :global_shared_files, %w(public/uploads)
 set :deploy_to, '/home/deploy/vitrinas-back'
-set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-set :keep_releases, 5
-set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.5.1' # Edit this if you are using MRI Ruby
-
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :shared_path, '/home/deploy/vitrinas-back/shared'
+set :current_path, '/home/deploy/vitrinas-back/current'
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w(config/database.yml config/application.yml public/cache.json)
+set :puma_threads, [4, 16]
+set :puma_workers, 6
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 8]
-set :puma_workers, 0
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
+set :puma_init_active_record, false  # Change to false when not using ActiveRecord
+set :rvm_type, :user
+set :rvm_ruby_version, 'ruby-2.5.1' # Edit this if you are using MRI Ruby
