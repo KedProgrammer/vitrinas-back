2cf1520e9cd2d5df6a7019e3d190fc85caafeaf9
Daniel <shirixu1@gmail.com>
2019-08-28 22:31:21 -0500

    added gemlock

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :current_path, '/home/deploy/vitrinas-back/current'
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :puma_threads, [4, 16]
 set :puma_workers, 6
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
