b602942e11c355d543cc403faf48d62053fd1763
Daniel <shirixu1@gmail.com>
2019-08-28 22:57:29 -0500

    added gemlock

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,13 @@ lock '3.11.0'
 set :application, 'vitrinas-back'
 set :repo_url, 'git@github.com:KedProgrammer/vitrinas-back.git'
 set :global_shared_files, %w(public/uploads)
-set :deploy_to, '/home/deploy/vitrinas-back'
-set :shared_path, '/home/deploy/vitrinas-back/shared'
-set :current_path, '/home/deploy/vitrinas-back/current'
+set :deploy_to, "/home/deploy/#{fetch(:application)}"
+set :shared_path, "/home/deploy/#{fetch(:application)}/shared"
+set :current_path, "/home/deploy/#{fetch(:application)}/current"
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :puma_threads, [4, 16]
 set :puma_workers, 6
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
