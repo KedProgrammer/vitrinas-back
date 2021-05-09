13854cfc2237af11029aaf59e6d56ab7163ef9b0
Daniel Tobon <daniel.tobon.c@yello.co>
2021-05-09 16:39:46 -0500

    tmp

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
+set :environment, "development"
+set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
+set :bundle_command, 'bundle exec'
+
 every 1.minute do # Use any day of the week or :weekend, :weekday
-  rake "create_log"
+  runner "EmailLog.create"
 end
