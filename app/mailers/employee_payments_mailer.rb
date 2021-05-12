8849ec5eee457c5bfb0a938a0701ccba6ced47ca
Daniel Tobon <daniel.tobon.c@yello.co>
2021-05-11 20:34:11 -0500

    added doris to email

--- app/mailers/employee_payments_mailer.rb
+++ app/mailers/employee_payments_mailer.rb
@@ -1,6 +1,6 @@
 class EmployeePaymentsMailer < ApplicationMailer
   def notify_payment
     attachments['reporte_cuotas.xls'] = File.read( Rails.root + "tmp/exports/reporte_cuotas.xls")
-    mail(to: 'shiriux1@gmail.com', subject: 'Notificacion de pago de cuotas')
+    mail(to: 'shiriux1@gmail.com, doristobongue@hotmail.com', subject: 'Notificacion de pago de cuotas')
   end
 end
