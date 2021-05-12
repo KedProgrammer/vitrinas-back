95cc970785a23fd1120fb9b4bdbfa12db663e17f
Daniel Tobon <daniel.tobon.c@yello.co>
2021-05-11 20:31:36 -0500

    added schedule for run payment

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ env :PATH, ENV['PATH']
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 set :bundle_command, 'bundle exec'
 
-every 1.minute do # Use any day of the week or :weekend, :weekday
-  runner "EmailLog.create"
+every :friday, at: '6am' do # Use any day of the week or :weekend, :weekday
+  runner "Payments::PayFees.call"
 end
