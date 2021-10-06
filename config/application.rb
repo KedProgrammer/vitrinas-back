38d052bdd33ce3d4b4be3cb9d40c84890a231bf9
Daniel Tobon <daniel.tobon.c@yello.co>
2021-10-05 20:39:23 -0500

    capistrano changes3

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 
-
-require 'sprockets/railtie'
 Bundler.require(*Rails.groups)
 
 module Orders
